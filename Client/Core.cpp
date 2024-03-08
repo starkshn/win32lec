e4a48d8a9567663e9f7997785e0ec808c1d4140e
ny <starkshn97@gmail.com>
2024-03-08 16:12:55 +0900

    12 double buffering -> Bitmap생성후 swap chain식으로 왔다갔다 연결

--- Client/Core.cpp
+++ Client/Core.cpp
@@ -11,7 +11,11 @@ Core::Core()
 
 Core::~Core()
 {
+	// Release...
 	ReleaseDC(_hwnd, _hdc);
+
+	DeleteDC(_memdc);
+	DeleteObject(_hbit);
 }
 
 int Core::Init(HWND hWnd, POINT resolution)
@@ -19,32 +23,21 @@ int Core::Init(HWND hWnd, POINT resolution)
 	_hwnd = hWnd;
 	_resolution = resolution;
 
-	// Window Size Calculate
-	RECT rc = {0, 0, _resolution .x, _resolution .y};
-	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
-
-	int cx = rc.right - rc.left;
-	int cy = rc.bottom - rc.top;
-	SetWindowPos(_hwnd, nullptr, 300, 100, cx, cy, 0);
-
-	_hdc = GetDC(_hwnd);
+	InitWindow();
+	InitManager();
+	InitBitMap();
 
 	obj.SetScale(Vec2{ 100, 100 });
 	obj.SetPos(Vec2{ int(_resolution.x / 2), int(_resolution.x  / 2)});
-
-	// Manager
-	GET_SINGLE(KeyManager)->Init();
-	GET_SINGLE(TimeManager)->Init();
 
 	return S_OK;
 }
 
 void Core::Progress()
 {
-	// Manager Updates
-	GET_SINGLE(KeyManager)->Update();
-	GET_SINGLE(TimeManager)->Update();
+	UpdateManager();
 
+	// Core
 	Update();
 	Render();
 }
@@ -68,12 +61,68 @@ void Core::Update()
 
 void Core::Render()
 {
+	RenderBegin();
+	Rendering();
+	RenderEnd();
+}
+
+void Core::UpdateManager()
+{
+	// Manager Updates
+	GET_SINGLE(KeyManager)->Update();
+	GET_SINGLE(TimeManager)->Update();
+}
+
+void Core::RenderBegin()
+{
+	// clear bitmap
 	Rectangle
+	(_memdc, -1, -1, _resolution.x + 1, _resolution.y + 1);
+}
+
+void Core::Rendering()
+{
+	// render objects (draw objects)
+	Rectangle(_memdc, obj.GL, obj.GT, obj.GR, obj.GB);
+}
+
+void Core::RenderEnd()
+{
+	// copy prev to main window
+	BitBlt
 	(
-		_hdc, 
-		obj.GL,
-		obj.GT,
-		obj.GR,
-		obj.GB
+		_hdc,
+		0, 0, _resolution.x, _resolution.y, 
+		_memdc, 0, 0, SRCCOPY
 	);
+}
+
+void Core::InitWindow()
+{
+	// Window Size Calculate
+	RECT rc = { 0, 0, _resolution.x, _resolution.y };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
+
+	int cx = rc.right - rc.left;
+	int cy = rc.bottom - rc.top;
+	SetWindowPos(_hwnd, nullptr, 300, 100, cx, cy, 0);
+
+	_hdc = GetDC(_hwnd);
+}
+
+void Core::InitManager()
+{
+	GET_SINGLE(KeyManager)->Init();
+	GET_SINGLE(TimeManager)->Init();
+}
+
+void Core::InitBitMap()
+{
+	// double buffering
+	_hbit = CreateCompatibleBitmap(_hdc, _resolution.x, _resolution.y);
+	_memdc = CreateCompatibleDC(_hdc);
+
+	// ±³Ã¼
+	HBITMAP prevBit = (HBITMAP)SelectObject(_memdc, _hbit);
+	DeleteObject(prevBit);
 }
--- Client/Core.h
+++ Client/Core.h
@@ -12,14 +12,29 @@ public:
 	int		Init(HWND hWnd, POINT resolution);
 	void	Progress();
 
-	const HWND	GetHwnd() { return _hwnd; }
-
+	const HWND GetHwnd() { return _hwnd; }
+	
 private:
 	void Update();
 	void Render();
+	
+private:
+	void UpdateManager();
 
+	void RenderBegin();
+	void Rendering();
+	void RenderEnd();
+	
+private:
+	void InitWindow();
+	void InitManager();
+	void InitBitMap();
+	
 private:
 	HWND	_hwnd = 0;
 	POINT	_resolution = {};
 	HDC		_hdc = 0;
+	
+	HBITMAP _hbit = 0;
+	HDC		_memdc = 0;
 };
